f349efead8ab568f75c507be74f2d043c1b3b883
Hyeonjin Park <97075204+hgggny@users.noreply.github.com>
2023-06-26 22:26:16 +0900

    [Bronze V] Title: ??!, Time: 216 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String id = "";
+
+        Scanner sc = new Scanner(System.in);
+        id = sc.next();
+        System.out.println(id + "??!");
+    }
+}
