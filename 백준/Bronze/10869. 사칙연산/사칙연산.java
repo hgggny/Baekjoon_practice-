30a2d7f9f9beabb5b42354cd66a6a41a868f764f
Hyeonjin Park <97075204+hgggny@users.noreply.github.com>
2023-06-26 21:46:25 +0900

    [Bronze V] Title: 사칙연산, Time: 200 ms, Memory: 17584 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.next());
+        int b = Integer.parseInt(scanner.next());
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
