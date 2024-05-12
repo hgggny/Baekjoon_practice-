7740067e9a9629cc4b33e6acc64a98214c72c163
hyunzinbak <97075204+hgggny@users.noreply.github.com>
2024-05-12 17:49:05 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.37 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] emergency) {
+        int[] asc = emergency.clone();
+        int[] answer = new int[emergency.length];
+        
+        Arrays.sort(asc);
+        
+        for(int i=0; i<asc.length; i++) {
+            for(int j=0; j<emergency.length; j++) {
+                if(asc[i]==emergency[j]) {
+                    answer[j]=emergency.length-i;
+                }
+            }
+        }
+        return answer;
+    }
+}
