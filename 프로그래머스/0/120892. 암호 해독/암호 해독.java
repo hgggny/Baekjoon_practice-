eebcfef588abbce824842352940c815515d146d0
hyunzinbak <97075204+hgggny@users.noreply.github.com>
2024-06-23 10:41:26 +0900

    [level 0] Title: 암호 해독, Time: 2.34 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,11 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Solution {
+    public String solution(String cipher, int code) {
+        return IntStream.range(0, cipher.length())
+                        .filter(i -> (i + 1) % code == 0)
+                        .mapToObj(i -> String.valueOf(cipher.charAt(i)))
+                        .collect(Collectors.joining());
+    }
+}
